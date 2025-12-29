5cbd8a3c33559f1286cd0d9679f9d94d22324ea8
Mbola14 <mandriamifidimanana@gmail.com>
2025-12-29 18:33:36 +0100

    Configurer vite pour build un widget Zoho CRM

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",                // IMPORTANT pour que les assets marchent dans Zoho
+  build: {
+    outDir: "../app",        // on build directement dans le dossier widget
+    emptyOutDir: false,      // IMPORTANT pour ne pas supprimer app/translations
+  },
+});
